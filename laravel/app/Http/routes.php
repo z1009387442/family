7bd7488053f2847505a6106c5c2b42663c06d880
zmm12 <920417690@qq.com>
2017-09-08 19:40:01 +0800

    前台评论展示成功

--- laravel/app/Http/routes.php
+++ laravel/app/Http/routes.php
@@ -146,6 +146,11 @@ Route::group(['middleware' => ['web']], function () {
 				'uses'=>'HotelController@room',
 				])->where(['id'=>'[0-9]+']);
 
+				//前台酒店评价
+				Route::any('hotel/assess',[
+				'uses'=>'HotelController@assess',
+				]);
+
 				//前台关于团队展示
 				Route::any('hotel/room',[
 				'uses'=>'HotelController@room',
---
+++ laravel/database/migrations/2017_09_07_132123_create_assess_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAssessTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('assess', function (Blueprint $table) {
+            $table->increments('assess_id');
+            $table->string('assess_desc');//
+            $table->Integer('assess_num');
+            $table->smallInteger('user_id');
+            $table->smallInteger('hotel_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('assess');
+    }
+}
