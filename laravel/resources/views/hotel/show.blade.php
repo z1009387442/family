2bf871ae546013f1ca0ef5bc78562b3b28d6a045
z1009387442 <1009387442@qq.com>
2017-09-12 16:21:32 +0800

    设施搜索

--- laravel/resources/views/hotel/show.blade.php
+++ laravel/resources/views/hotel/show.blade.php
@@ -33,7 +33,7 @@
         background-repeat: no-repeat;
         }
 .p29_0_0,.p29_0_1,.p29_0_2,.p29_0_3,.p29_0_4,.p29_0_5,.p29_0_6,.p29_0_7,.p29_0_8,.p29_0_9,.p29_0_10{background-image: url('/Blur/Pic?b=ba37f0a457ac4c0494c8a0233d8afe0d'); font-size: 27px } .p29_0_0{ background-position: -4px -4px; padding: 0 8px } .p29_0_1{ background-position: -51px -4px; padding: 0 8px } .p29_0_2{ background-position: -109px -4px; padding: 0 8px } .p29_0_3{ background-position: -171px -4px; padding: 0 8px } .p29_0_4{ background-position: -196px -4px; padding: 0 8px } .p29_0_5{ background-position: -258px -4px; padding: 0 8px } .p29_0_6{ background-position: -305px -4px; padding: 0 4px } .p29_0_7{ background-position: -341px -4px; padding: 0 8px } .p29_0_8{ background-position: -389px -4px; padding: 0 8px } .p29_0_9{ background-position: -420px -4px; padding: 0 8px } .p29_0_10{ background-position: -489px -4px; padding: 0 8px } .p18_0_0,.p18_1_0,.p18_0_1,.p18_1_1,.p18_0_2,.p18_1_2,.p18_0_3,.p18_1_3,.p18_0_4,.p18_1_4,.p18_0_5,.p18_1_5,.p18_0_6,.p18_1_6,.p18_0_7,.p18_1_7,.p18_0_8,.p18_1_8,.p18_0_9,.p18_1_9,.p18_0_10,.p18_1_10{background-image: url('/Blur/Pic?b=d1902153e21048c58eaf4e0e838aff69'); font-size: 16px } .p18_0_0{ background-position: -2px -1px; padding: 0 5px } .p18_1_0{ background-position: -2px -21px; padding: 0 5px } .p18_0_1{ background-position: -25px -1px; padding: 0 5px } .p18_1_1{ background-position: -25px -21px; padding: 0 5px } .p18_0_2{ background-position: -42px -1px; padding: 0 5px } .p18_1_2{ background-position: -42px -21px; padding: 0 5px } .p18_0_3{ background-position: -64px -1px; padding: 0 5px } .p18_1_3{ background-position: -64px -21px; padding: 0 5px } .p18_0_4{ background-position: -84px -1px; padding: 0 5px } .p18_1_4{ background-position: -84px -21px; padding: 0 5px } .p18_0_5{ background-position: -114px -1px; padding: 0 5px } .p18_1_5{ background-position: -114px -21px; padding: 0 5px } .p18_0_6{ background-position: -144px -1px; padding: 0 2px } .p18_1_6{ background-position: -144px -21px; padding: 0 2px } .p18_0_7{ background-position: -159px -1px; padding: 0 5px } .p18_1_7{ background-position: -159px -21px; padding: 0 5px } .p18_0_8{ background-position: -174px -1px; padding: 0 5px } .p18_1_8{ background-position: -174px -21px; padding: 0 5px } .p18_0_9{ background-position: -188px -1px; padding: 0 5px } .p18_1_9{ background-position: -188px -21px; padding: 0 5px } .p18_0_10{ background-position: -215px -1px; padding: 0 5px } .p18_1_10{ background-position: -215px -21px; padding: 0 5px } .p16_0_0,.p16_1_0,.p16_0_1,.p16_1_1,.p16_0_2,.p16_1_2,.p16_0_3,.p16_1_3,.p16_0_4,.p16_1_4,.p16_0_5,.p16_1_5,.p16_0_6,.p16_1_6,.p16_0_7,.p16_1_7,.p16_0_8,.p16_1_8,.p16_0_9,.p16_1_9,.p16_0_10,.p16_1_10{background-image: url('/Blur/Pic?b=8abba637b7ba46ce9a25dd009f960189'); font-size: 15px } .p16_0_0{ background-position: -2px -2px; padding: 0 4px } .p16_1_0{ background-position: -2px -20px; padding: 0 4px } .p16_0_1{ background-position: -32px -2px; padding: 0 4px } .p16_1_1{ background-position: -32px -20px; padding: 0 4px } .p16_0_2{ background-position: -66px -2px; padding: 0 4px } .p16_1_2{ background-position: -66px -20px; padding: 0 4px } .p16_0_3{ background-position: -91px -2px; padding: 0 4px } .p16_1_3{ background-position: -91px -20px; padding: 0 4px } .p16_0_4{ background-position: -121px -2px; padding: 0 4px } .p16_1_4{ background-position: -121px -20px; padding: 0 4px } .p16_0_5{ background-position: -154px -2px; padding: 0 4px } .p16_1_5{ background-position: -154px -20px; padding: 0 4px } .p16_0_6{ background-position: -181px -2px; padding: 0 2px } .p16_1_6{ background-position: -181px -20px; padding: 0 2px } .p16_0_7{ background-position: -199px -2px; padding: 0 4px } .p16_1_7{ background-position: -199px -20px; padding: 0 4px } .p16_0_8{ background-position: -213px -2px; padding: 0 4px } .p16_1_8{ background-position: -213px -20px; padding: 0 4px } .p16_0_9{ background-position: -251px -2px; padding: 0 4px } .p16_1_9{ background-position: -251px -20px; padding: 0 4px } .p16_0_10{ background-position: -273px -2px; padding: 0 4px } .p16_1_10{ background-position: -273px -20px; padding: 0 4px } .p14_0_0,.p14_0_1,.p14_0_2,.p14_0_3,.p14_0_4,.p14_0_5,.p14_0_6,.p14_0_7,.p14_0_8,.p14_0_9,.p14_0_10{background-image: url('/Blur/Pic?b=9b619e82e1384d8fa633432e91414433'); font-size: 13px } .p14_0_0{ background-position: -2px -2px; padding: 0 4px } .p14_0_1{ background-position: -28px -2px; padding: 0 4px } .p14_0_2{ background-position: -56px -2px; padding: 0 4px } .p14_0_3{ background-position: -77px -2px; padding: 0 4px } .p14_0_4{ background-position: -102px -2px; padding: 0 4px } .p14_0_5{ background-position: -130px -2px; padding: 0 4px } .p14_0_6{ background-position: -153px -2px; padding: 0 3px } .p14_0_7{ background-position: -169px -2px; padding: 0 4px } .p14_0_8{ background-position: -181px -2px; padding: 0 4px } .p14_0_9{ background-position: -213px -2px; padding: 0 4px } .p14_0_10{ background-position: -232px -2px; padding: 0 4px } .p12_0_0,.p12_1_0,.p12_0_1,.p12_1_1,.p12_0_2,.p12_1_2,.p12_0_3,.p12_1_3,.p12_0_4,.p12_1_4,.p12_0_5,.p12_1_5,.p12_0_6,.p12_1_6,.p12_0_7,.p12_1_7,.p12_0_8,.p12_1_8,.p12_0_9,.p12_1_9,.p12_0_10,.p12_1_10{background-image: url('/Blur/Pic?b=10c45e24f84a4e0fbefa9bb40efba0d6'); font-size: 12px } .p12_0_0{ background-position: -2px -3px; padding: 0 3px } .p12_1_0{ background-position: -2px -18px; padding: 0 3px } .p12_0_1{ background-position: -12px -3px; padding: 0 3px } .p12_1_1{ background-position: -12px -18px; padding: 0 3px } .p12_0_2{ background-position: -32px -3px; padding: 0 3px } .p12_1_2{ background-position: -32px -18px; padding: 0 3px } .p12_0_3{ background-position: -55px -3px; padding: 0 3px } .p12_1_3{ background-position: -55px -18px; padding: 0 3px } .p12_0_4{ background-position: -66px -3px; padding: 0 3px } .p12_1_4{ background-position: -66px -18px; padding: 0 3px } .p12_0_5{ background-position: -76px -3px; padding: 0 3px } .p12_1_5{ background-position: -76px -18px; padding: 0 3px } .p12_0_6{ background-position: -97px -3px; padding: 0 1px } .p12_1_6{ background-position: -97px -18px; padding: 0 1px } .p12_0_7{ background-position: -113px -3px; padding: 0 3px } .p12_1_7{ background-position: -113px -18px; padding: 0 3px } .p12_0_8{ background-position: -124px -3px; padding: 0 3px } .p12_1_8{ background-position: -124px -18px; padding: 0 3px } .p12_0_9{ background-position: -153px -3px; padding: 0 3px } .p12_1_9{ background-position: -153px -18px; padding: 0 3px } .p12_0_10{ background-position: -168px -3px; padding: 0 3px } .p12_1_10{ background-position: -168px -18px; padding: 0 3px }     </style>
-<style type="text/css">.amap-container{cursor:url(http://webapi.amap.com/theme/v1.3/openhand.cur),default;}.amap-drag{cursor:url(http://webapi.amap.com/theme/v1.3/closedhand.cur),default;}</style><script charset="utf-8" src="/qiantai/search_style/v.htm"></script></head>
+<style type="text/css">.amap-container{cursor:url(http://webapi.amap.com/theme/v1.3/openhand.cur),default;}.amap-drag{cursor:url(http://webapi.amap.com/theme/v1.3/closedhand.cur),default;}</style></head>
 
 <body>
 <!-- loading -->
@@ -301,7 +301,7 @@
                 <div class="lbox">
                     <div id="Plist_hotel" class="Plist_hotel" data-nblock-id="block/hotelListHotels?initHotelList=hotelListData">
                         <div class="citycount">
-                            当前城市，为您找到<b>503</b>家酒店
+                           当前城市，为您找到<b>503</b>家酒店
                         </div>
                         <div class="filterbox">
                             <div class="filterbar Lcfx" style="top: 52px; left: 164.6px; position: static; width: auto;">
@@ -334,16 +334,16 @@
     <div class="hotelbox">
         
         
-        <div class="img"><img src="<?php echo $hotel->hotel_img; ?>" width="190" height="160"></div>
+        <div class="img"><img src="<?php echo $hotel['hotel_img']; ?>" width="190" height="160"></div>
         <div class="desc">
             <div class="hotelname  hasTaxBedge">
-                <a class="name" title="<?php echo $hotel->hotel_name; ?>" href="{{url('home/hotel/room')}}/id/<?php echo $hotel->hotel_id; ?>" target="_blank">
-                    <h2><?php echo $hotel->hotel_name; ?></h2>
+                <a class="name" title="<?php echo $hotel['hotel_name']; ?>" href="{{url('home/hotel/room')}}/id/<?php echo $hotel['hotel_id']; ?>" target="_blank">
+                    <h2><?php echo $hotel['hotel_name']; ?></h2>
                 </a>
                 
                 <span class="tax_bedge tax_bedge_0">普票立取</span>
             </div>
-            <div class="address city"><?php echo $hotel->hotel_address; ?></div>
+            <div class="address city"><?php echo $hotel['hotel_address']; ?></div>
             
             
             <div class="lastorder"></div>
@@ -364,13 +364,13 @@
             </div>
         </div>
         <div class="rarea">
-            <div class="pricearea Ltar" data-hotel-lowest-price="-143"><span class="price"><i>￥</i><var class="p29_0_0"></var><var class="p29_0_4"></var><var class="p29_0_3"></var><i>起</i></span></div>
+            <div class="pricearea Ltar" data-hotel-lowest-price="-143"><span class="price"><i>￥</i><i><font size="6"><?php echo $hotel['price']; ?></font>起</i></span></div>
             <div class="score Ltar">
                 <i class="Cicon star full"></i><i class="Cicon star full"></i><i class="Cicon star full"></i><i class="Cicon star full"></i><i class="Cicon star full"></i>
                 <span class="Ldib Lpl5">5<i>/5分</i></span>
             </div>
-            <div class="comment Ltar"><a href="{{url('home/hotel/room')}}/id/<?php echo $hotel->hotel_id; ?>" target="_blank">共0条评论</a></div>
-            <div class="btnbox Ltar"><a href="{{url('home/hotel/room')}}/id/<?php echo $hotel->hotel_id; ?>" target="_blank" class="Cbtn viewexpand">查看详情</a></div>
+            <div class="comment Ltar"><a href="{{url('home/hotel/room')}}/id/<?php echo $hotel['hotel_id']; ?>" target="_blank">共0条评论</a></div>
+            <div class="btnbox Ltar"><a href="{{url('home/hotel/room')}}/id/<?php echo $hotel['hotel_id']; ?>" target="_blank" class="Cbtn viewexpand">查看详情</a></div>
             
             <div class="expandlink Ltar">
                 <a class="viewroomtype" href="javascript:;"><span>展开全部房型</span><i class="arrow"></i></a>
@@ -412,9 +412,9 @@
         <div data-nblock-id="block/hotelUserInfo"></div>
         <div data-nblock-id="block/trackCompatibleMode"></div>
 
-        <script src="/qiantai/search_style/hm.js"></script>
-        <script type="text/javascript" async="" src="/qiantai/search_style/piwik.js"></script>
-        <script type="text/javascript" async="" src="/qiantai/search_style/ga.js"></script><script type="text/javascript" async="" src="/qiantai/search_style/vds.js"></script><script type="text/javascript" src="/qiantai/search_style/mainApp.js" async="true"></script><script type="text/javascript" src="/qiantai/search_style/jquery.js" async="true"></script><script type="text/javascript" src="/qiantai/search_style/mainBlock.js" async="true"></script><script type="text/javascript" src="/qiantai/search_style/CheckinCityTemplate.js" async="true"></script><script type="text/javascript" src="/qiantai/search_style/CheckinCitySuggestTemplate.js" async="true"></script><script type="text/javascript" src="/qiantai/search_style/CheckinKeywordTemplate.js" async="true"></script><script type="text/javascript" src="/qiantai/search_style/CheckinKeySuggestTemplate.js" async="true"></script><script type="text/javascript" src="/qiantai/search_style/CheckinCityFixedTemplate.js" async="true"></script><script type="text/javascript" src="/qiantai/search_style/searchFilterList.js" async="true"></script><script type="text/javascript" src="/qiantai/search_style/VisitedHotelItem.js" async="true"></script><script type="text/javascript" src="/qiantai/search_style/checkinCity.js" async="true"></script><script type="text/javascript" src="/qiantai/search_style/checkinKeyword.js" async="true"></script><script type="text/javascript" src="/qiantai/search_style/visitedHotel.js" async="true"></script><link type="text/css" rel="stylesheet" href="/qiantai/search_style/jquery-ui.css"><link type="text/css" rel="stylesheet" href="/qiantai/search_style/jquery-ui_002.css"><script type="text/javascript" src="/qiantai/search_style/jquery-ui.js" async="true"></script><script type="text/javascript" src="/qiantai/search_style/HotelBox.js" async="true"></script><script type="text/javascript" src="/qiantai/search_style/RoomType.js" async="true"></script><script type="text/javascript" src="/qiantai/search_style/util.js" async="true"></script><script type="text/javascript" src="/qiantai/search_style/datepicker-zh-CN.js" async="true"></script><script src="/qiantai/search_style/library.js"></script>
+        
+       
+        
         <!--[if lte IE 9]>
             <script src="http://ws-www.hantinghotels.com/newweb/hotels/lib/jquery/jquery.placeholder.min.5fbd3e01.js"></script>
             <script>$('input, textarea').placeholder({ customClass: 'Cplaceholder' });</script>
